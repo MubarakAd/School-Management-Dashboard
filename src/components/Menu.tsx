a1c3d3cadc1fd5d0d35a451a383a5b33c264d489
MubarakAd <mubaarakadem@gmail.com>
2024-12-16 00:02:19 +0300

    add try.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,10 @@
+import { role } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
+
+// Mock role for demonstration, replace with actual role logic.
+
 const menuItems = [
   {
     title: "MENU",
@@ -115,28 +119,39 @@ const menuItems = [
     ],
   },
 ];
+
 const Menu = () => {
   return (
-    
-    <div className=' mt-2 text-sm'>
-      
-        {menuItems.map(i=>(
-          <div className='flex flex-col gap-2'  key={i.title}>
-            <span className='hidden lg:block text-gray-400 pl-2 font-light'>{i.title}</span>
-              {i.items.map(item=>(
-                <Link className='flex items-center justify-center lg:justify-start gap-4 py-2 lg:pl-5 ' key={item.label} href={item.href}>
-                  <Image src={item.icon} alt={item.label} width={18} height={18}/>
-                  <span className='hidden lg:block text-gray-500  hover:text-black '>{item.label}</span>
+    <div className='mt-2 text-sm'>
+      {menuItems.map((menu) => (
+        <div className='flex flex-col gap-2' key={menu.title}>
+          <span className='hidden lg:block text-gray-400 pl-2 font-light'>
+            {menu.title}
+          </span>
+          {menu.items.map(
+            (item) =>
+              item.visible.includes(role) && (
+                <Link
+                  className='flex items-center justify-center lg:justify-start gap-4 py-2 lg:pl-5'
+                  key={item.label}
+                  href={item.href}
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={18}
+                    height={18}
+                  />
+                  <span className='hidden lg:block text-gray-500 hover:text-black'>
+                    {item.label}
+                  </span>
                 </Link>
-
-              ))}
-  
-          </div>
-        ))}
-      
-
+              )
+          )}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
