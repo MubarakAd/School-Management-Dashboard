bc854bbe4950eed60059a853d286ae67a6ee86f2
MubarakAd <mubaarakadem@gmail.com>
2024-12-24 16:05:22 +0300

    add results page

---
+++ src/lib/resultsHeaderData.ts
@@ -0,0 +1,10 @@
+
+export const column=[
+    {key:"Subject Name",label:"Subject Name",className:"px-4 py-2 "},
+    {key:"Student",label:"Student",className:"px-4 py-2 "},
+    {key:"Score",label:"Score",className:"hidden md:table-cell px-4 py-2 "},
+    {key:"Teacher",label:"Teacher",className:"hidden md:table-cell px-4 py-2 "},
+    {key:"Class",label:"Class",className:"hidden md:table-cell px-4 py-2 "},
+    {key:"Date",label:"Date",className:"hidden md:table-cell px-4 py-2 "},
+    {key:"Actions",label:"Actions",className:"px-4 py-2"}
+]
