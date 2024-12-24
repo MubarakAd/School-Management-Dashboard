882e4e8eaa5a1b2384a350f3b63eae05c10f710e
MubarakAd <mubaarakadem@gmail.com>
2024-12-24 15:30:36 +0300

    add exams page and humburger icon for small screen

---
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import CommonHeader from "@/components/CommonHeader/CommonHearder";
+import DataTable from "@/components/DataTable/DataTable";
+import { role, examsData} from "@/lib/data";
+import { column } from "@/lib/examsHeaderData";
+import Image from "next/image";
+import Link from "next/link";
+export interface rowType {
+    id: number,
+    subject: string,
+    class: string,
+    teacher: string,
+    date: string,
+}
+const render = (item: rowType) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-50"
+  >
+    <td className=" p-4 " >{item.subject}</td>
+    <td className="hidden md:table-cell p-4 ">{item.class}</td>
+    <td className="hidden md:table-cell p-4 " >{item.teacher}</td>
+    <td className="hidden md:table-cell p-4 " >{item.date}</td>
+    <td className="flex p-4   gap-4">
+      <Link href={`/list/teachers/${item.id}`}>
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-teal-300">
+          <Image src={"/view.png"} alt="" width={16} height={16} />
+        </button>
+      </Link>
+      {role === "admin" && (
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-purple-300">
+          <Image src={"/delete.png"} alt="" width={16} height={16} />
+        </button>
+      )}
+    </td>
+  </tr>
+);
+const page = () => {
+  return (
+    <div className="p-4 bg-white m-3 rounded-md">
+      <CommonHeader career="All Exams" />
+      <DataTable data={examsData} columns={column} render={render} />
+    </div>
+  );
+};
+
+export default page;
